a3d08565372c3374612e0d33c53902d7da097f41
AllanJones24 <allanjonesjr24@gmail.com>
2023-12-11 23:16:37 -0600

    Added list of levels and (all balloons popped) condition

--- Assets/__Scripts/Main.cs
+++ Assets/__Scripts/Main.cs
@@ -1,23 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour
 {
     static public int balloonCount;
     static public int lives;
+    private GameObject currentLevel;
+
+    [System.Serializable]
+    public class Levels
+    {
+        public GameObject levelPrefab;
+    }
+
+    public List<Levels> levels;
+    private int currentLevelIndex = 0;
+
     private void Start()
     {
-        startLevel();
-        GameObject[] balloons = GameObject.FindGameObjectsWithTag("Balloon");
-        balloonCount = balloons.Length;
+        LoadLevel();
     }
+
     private void Update()
     {
-
+        // Check if all balloons are popped
+        if (balloonCount == 0)
+        {
+            // Move to the next level if available
+            if (currentLevelIndex + 1 < levels.Count)
+            {
+                currentLevelIndex++;
+                LoadLevel();
+            }
+            else
+            {
+                // All levels completed (you can handle this as needed)
+                Debug.Log("All levels completed!");
+            }
+        }
     }
+
+    private void LoadLevel()
+    {
+        // Destroy previous level objects
+        GameObject[] previousBalloons = GameObject.FindGameObjectsWithTag("Balloon");
+        foreach (GameObject balloon in previousBalloons)
+        {
+            Destroy(balloon);
+        }
+
+        // Deactivate the previous level
+        if (currentLevel != null)
+        {
+            currentLevel.SetActive(false);
+        }
+
+        // Load the new level
+        currentLevel = Instantiate(levels[currentLevelIndex].levelPrefab);
+        balloonCount = GameObject.FindGameObjectsWithTag("Balloon").Length;
+        startLevel();
+    }
+
+
+
+
     private void startLevel()
     {
         lives = 3;
     }
 }
+
